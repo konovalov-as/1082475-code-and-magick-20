871290890753f4702e84611f97df7126eda8e275
Konovalov Alexander <konovalov_a.s@mail.ru>
2020-06-16 07:55:12 +0500

    Добавляет исправления

--- js/setup.js
+++ js/setup.js
@@ -15,14 +15,9 @@
 
   // кнопка закрытия окна настроек персонажа
   var setupClose = setup.querySelector('.setup-close');
-  setupClose.setAttribute('tabindex', '0');
 
   // кнопка открытия окна настроек персонажа
   var setupOpen = document.querySelector('.setup-open');
-  var setupOpenIcon = document.querySelector('.setup-open-icon');
-  if (setupOpenIcon) {
-    setupOpenIcon.setAttribute('tabindex', '0');
-  }
 
   var removeClass = function (element, className) {
     element.classList.remove(className);
@@ -34,140 +29,115 @@
 
   // нажатие Esc
   var onPopupEscPress = function (evt) {
-    if (evt.key === 'Escape') {
-      evt.preventDefault();
-      closePopup();
+    var KEY = 'Escape';
+    var activeTagName = 'INPUT';
+    if (evt.target.tagName === activeTagName) {
+      return;
+    } else {
+      if (evt.key === KEY) {
+        evt.preventDefault();
+        closePopup();
+      }
     }
   };
 
   // открывает модальное окно
   var openPopup = function () {
-    if (setup) {
-      removeClass(setup, HIDDEN_CLASS);
-      setupOpen.addEventListener('keydown', onPopupEscPress);
+    removeClass(setup, HIDDEN_CLASS);
+    document.addEventListener('keydown', onPopupEscPress);
 
-      // форма
-      var setupWizardForm = setup.querySelector('.setup-wizard-form');
-      if (setupWizardForm) {
-        setupWizardForm.setAttribute('action', 'https://javascript.pages.academy/code-and-magick');
+    // форма
+    var setupWizardForm = setup.querySelector('.setup-wizard-form');
+
+    // поле ввода имени персонажа
+    var setupUserName = setupWizardForm.querySelector('.setup-user-name');
+
+    // замена стандартных сообщений
+    setupUserName.addEventListener('invalid', function () {
+      if (setupUserName.validity.tooShort) {
+        setupUserName.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+      } else if (setupUserName.validity.tooLong) {
+        setupUserName.setCustomValidity('Имя не должно превышать 25-ти символов');
+      } else if (setupUserName.validity.valueMissing) {
+        setupUserName.setCustomValidity('Обязательное поле');
+      } else {
+        setupUserName.setCustomValidity('');
       }
+    });
 
-      // поле ввода имени персонажа
-      var setupUserName = setupWizardForm.querySelector('.setup-user-name');
-      if (setupUserName) {
-        setupUserName.setAttribute('minlength', '2');
-        setupUserName.setAttribute('maxlength', '25');
+    // вывод сообщений при изменении данных в поле
+    setupUserName.addEventListener('input', function () {
+      var valueLength = setupUserName.value.length;
 
-        // замена стандартных сообщений
-        setupUserName.addEventListener('invalid', function () {
-          if (setupUserName.validity.tooShort) {
-            setupUserName.setCustomValidity('Имя должно состоять минимум из 2-х символов');
-          } else if (setupUserName.validity.tooLong) {
-            setupUserName.setCustomValidity('Имя не должно превышать 25-ти символов');
-          } else if (setupUserName.validity.valueMissing) {
-            setupUserName.setCustomValidity('Обязательное поле');
-          } else {
-            setupUserName.setCustomValidity('');
-          }
-        });
-
-        // вывод сообщений при изменении данных в поле
-        setupUserName.addEventListener('input', function () {
-          var valueLength = setupUserName.value.length;
-
-          if (valueLength < MIN_NAME_LENGTH) {
-            setupUserName.setCustomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) + ' симв.');
-          } else if (valueLength > MAX_NAME_LENGTH) {
-            setupUserName.setCustomValidity('Удалите лишние ' + (valueLength - MIN_NAME_LENGTH) + ' симв.');
-          } else {
-            setupUserName.setCustomValidity('');
-          }
-        });
+      if (valueLength < MIN_NAME_LENGTH) {
+        setupUserName.setCustomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) + ' симв.');
+      } else if (valueLength > MAX_NAME_LENGTH) {
+        setupUserName.setCustomValidity('Удалите лишние ' + (valueLength - MIN_NAME_LENGTH) + ' симв.');
+      } else {
+        setupUserName.setCustomValidity('');
       }
+    });
 
-      // поля автарки
-      var avatar = setupWizardForm.querySelector('input[name=avatar');
-      if (avatar) {
-        avatar.setAttribute('accept', 'image/png, image/jpeg');
-      }
+    // скрытые поля формы с цветами
+    var hiddenCoatColor = setupWizardForm.querySelector('input[name=coat-color]');
+    var hiddenEyesColor = setupWizardForm.querySelector('input[name=eyes-color]');
+    var hiddenFireballColor = setupWizardForm.querySelector('input[name=fireball-color]');
 
-      // скрытые поля формы с цветами
-      var hiddenCoatColor = setupWizardForm.querySelector('input[name=coat-color]');
-      var hiddenEyesColor = setupWizardForm.querySelector('input[name=eyes-color]');
-      var hiddenFireballColor = setupWizardForm.querySelector('input[name=fireball-color]');
+    // мантия персонажа
+    var wizardCoat = setupWizardForm.querySelector('.wizard-coat');
+    // меняет цвет мантии персонажа по клику
+    wizardCoat.addEventListener('click', function () {
+      var randomCoatColor = COAT_COLORS[getRandomNumber(0, COAT_COLORS.length - 1)];
+      wizardCoat.style.fill = randomCoatColor;
+      hiddenCoatColor.value = randomCoatColor;
+    });
 
-      // мантия персонажа
-      var wizardCoat = setupWizardForm.querySelector('.wizard-coat');
-      // меняет цвет мантии персонажа по клику
-      if (wizardCoat) {
-        wizardCoat.addEventListener('click', function () {
-          var randomCoatColor = COAT_COLORS[getRandomNumber(0, COAT_COLORS.length - 1)];
-          wizardCoat.style.fill = randomCoatColor;
-          if (hiddenCoatColor) {
-            hiddenCoatColor.value = randomCoatColor;
-          }
-        });
-      }
+    // глаза персонажа
+    var wizardEyes = setupWizardForm.querySelector('.wizard-eyes');
+    // меняет цвет глаз персонажа по клику
+    wizardEyes.addEventListener('click', function () {
+      var randomEyesColor = EYES_COLORS[getRandomNumber(0, EYES_COLORS.length - 1)];
+      wizardEyes.style.fill = randomEyesColor;
+      hiddenEyesColor.value = randomEyesColor;
+    });
 
-      // глаза персонажа
-      var wizardEyes = setupWizardForm.querySelector('.wizard-eyes');
-      // меняет цвет глаз персонажа по клику
-      if (wizardEyes) {
-        wizardEyes.addEventListener('click', function () {
-          var randomEyesColor = EYES_COLORS[getRandomNumber(0, EYES_COLORS.length - 1)];
-          wizardEyes.style.fill = randomEyesColor;
-          if (hiddenEyesColor) {
-            hiddenEyesColor.value = randomEyesColor;
-          }
-        });
-      }
-
-      // фаербол персонажа
-      var wizardFireball = setupWizardForm.querySelector('.setup-fireball-wrap');
-      // меняет цвет фаербола персонажа по клику
-      if (wizardFireball) {
-        wizardFireball.addEventListener('click', function () {
-          var randomFireballColor = FIREBALL_COLORS[getRandomNumber(0, FIREBALL_COLORS.length)];
-          wizardFireball.style.backgroundColor = randomFireballColor;
-          if (hiddenFireballColor) {
-            hiddenFireballColor.value = randomFireballColor;
-          }
-        });
-      }
-    }
+    // фаербол персонажа
+    var wizardFireball = setupWizardForm.querySelector('.setup-fireball-wrap');
+    // меняет цвет фаербола персонажа по клику
+    wizardFireball.addEventListener('click', function () {
+      var randomFireballColor = FIREBALL_COLORS[getRandomNumber(0, FIREBALL_COLORS.length)];
+      wizardFireball.style.backgroundColor = randomFireballColor;
+      hiddenFireballColor.value = randomFireballColor;
+    });
   };
 
   // закрывает модальное окно и удаляет обработчик
   var closePopup = function () {
-    if (setup) {
-      addClass(setup, HIDDEN_CLASS);
-      setupOpen.removeEventListener('keydown', onPopupEscPress);
-    }
+    addClass(setup, HIDDEN_CLASS);
+    document.removeEventListener('keydown', onPopupEscPress);
   };
 
   // открывает модальное окно по клику и нажатию Enter на автарке пользователя
-  if (setupOpen) {
-    setupOpen.addEventListener('click', openPopup);
-    setupOpen.addEventListener('keydown', function (evt) {
-      if (evt.key === 'Enter') {
-        evt.preventDefault();
-        openPopup();
-      }
-    });
+  setupOpen.addEventListener('click', openPopup);
+  setupOpen.addEventListener('keydown', function (evt) {
+    var KEY = 'Enter';
+    if (evt.key === KEY) {
+      evt.preventDefault();
+      openPopup();
+    }
+  });
 
-    // закрывает модельное окно по нажатию Enter на кнопке закрытия
-    setupClose.addEventListener('keydown', function (evt) {
-      if (evt.key === 'Enter') {
-        evt.preventDefault();
-        closePopup();
-      }
-    });
-  }
+  // закрывает модельное окно по нажатию Enter на кнопке закрытия
+  setupClose.addEventListener('keydown', function (evt) {
+    var KEY = 'Enter';
+    if (evt.key === KEY) {
+      evt.preventDefault();
+      closePopup();
+    }
+  });
 
   // закрывает окно по клику
-  if (setupClose) {
-    setupClose.addEventListener('click', closePopup);
-  }
+  setupClose.addEventListener('click', closePopup);
 
   // блок для вставки похожих персонажей
   var similarListElement = document.querySelector('.setup-similar-list');
@@ -232,14 +202,14 @@
   };
 
   // выводим блок персонажа на страницу
-  var renderWizard = function () {
+  var renderWizards = function () {
     var fragment = document.createDocumentFragment();
     for (var j = 0; j < similarWizards.length; j++) {
       fragment.appendChild(createWizard(similarWizards[j]));
     }
     similarListElement.appendChild(fragment);
   };
-  renderWizard();
+  renderWizards();
 
   var similarDialog = document.querySelector('.setup-similar');
   if (similarDialog) {
