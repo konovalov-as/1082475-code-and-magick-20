44f7d8d80608bbb2ab726f62ad65f52ce8487dc3
Коновалов Александр Сергеевич <konovalov_a.s@mail.ru>
2020-06-19 09:51:22 +0500

    Начинает разделять код на модули

---
+++ js/dialog.js
@@ -0,0 +1,6 @@
+'use strict';
+
+(function () {
+  console.log(window.util.ESC_KEY);
+  console.log(window.util.ENTER_KEY);
+})();
---
+++ js/util.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function () {
+  var ESC_KEY = 'Escape';
+  var ENTER_KEY = 'Enter';
+
+  window.util = {
+    ESC_KEY: ESC_KEY,
+    ENTER_KEY: ENTER_KEY,
+  }
+
+})();
